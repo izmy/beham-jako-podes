6e80fb6df0aa8bf2cddb8314ad7ced19ceed12d5
Jaroslav Hrách <me@jaroslavhrach.cz>
2024-12-17 21:37:18 +0100

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Běhám jako poděs",
@@ -18,6 +19,9 @@ export default function RootLayout({
       <body>
         <Header />
         <div className="container">{children}</div>
+        {process.env.GOOGLE_ANALYTICS_ID && (
+          <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID} />
+        )}
         <Footer />
       </body>
     </html>
